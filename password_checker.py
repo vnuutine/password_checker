3fb0a644f3d270e55a3ee10fae5ff4cdc8e9c125
Ville Nuutinen <ville.nuutinen@proton.me>
2025-12-23 20:07:44 +0200

    fix: code indentation, boolean return logic
    Renamed and simplified password check functions for clarity and conciseness. Replaced the main function with check_password, updated function calls, and improved boolean return logic. Cleaned up code structure for better readability and maintainability.

--- password_checker.py
+++ password_checker.py
@@ -2,55 +2,47 @@
 
 # Function to check the length of the string passed as parameter. Returns a boolean value.
 # len() takes the string and turns the length into a integer.
-def pswd_length(password):
-        length = len(password)
-        if length < 8:
-            return False
-        else: 
-            return True
+def has_min_length(password):
+    length = len(password)
+    if length < 8:
+        return False
+    else: 
+        return True
 
 
 # Function for checking if the parameter string contains atleast one digit. Returns a boolean value.
-def is_digit(password):
-        digit_found = False
+def has_digit(password):
+    digit_found = False
 
-        for c in password:
-            if c.isdigit():
-                digit_found = True
-                break
+    for c in password:
+        if c.isdigit():
+            digit_found = True
+            break
 
-        if digit_found == True:
-            return True
-        else:
-            return False
+    return digit_found
         
 # Function for checking if the parameter string contains atleast one uppercase letter. Returns a boolean value.
 def has_uppercase(password):
-        upper_case_found = False
+    upper_case_found = False
         
-        for c in password:
-            if c.isupper():
-                upper_case_found = True
-                break
+    for c in password:
+        if c.isupper():
+            upper_case_found = True
+            break
         
-        if upper_case_found == True:
-            return True
-        else: 
-            return False
+    return upper_case_found
 
 #  Function for checking if the parameter string contains atleast one lowercase letter. Returns a boolean value.
 def has_lowercase(password):
-        lower_case_found = False
+    lower_case_found = False
         
-        for c in password:
-            if c.islower():
-                lower_case_found = True
-                break
+    for c in password:
+        if c.islower():
+            lower_case_found = True
+            break
         
-        if lower_case_found == True:
-            return True
-        else: 
-            return False
+    return lower_case_found
+    
 # Function for checking if the parameter string contains atleast one of the symbols in the list. Returns a boolean value.
 def has_symbol(password):
     symbols = ["!", "@", "#", "$", "%", "^", "&", "*"]
@@ -61,48 +53,45 @@ def has_symbol(password):
             symbol_found = True
             break
     
-    if symbol_found == True:
-        return True
-    else:
-        return False
+    return symbol_found
         
 
 # Main function collecting the results of the check functions in to a dictionary and printing the password strength.
 # Also loops through the check function results, giving hints from dictionary.
-def main(password):
-        results = { "long enough": pswd_length(password),
-                    "has digits": is_digit(password),
-                    "has uppercase": has_uppercase(password),
-                    "has lowercase": has_lowercase(password),
-                    "has symbol": has_symbol(password)}
+def check_password(password):
+    results = { "long enough": has_min_length(password),
+                "has digits": has_digit(password),
+                "has uppercase": has_uppercase(password),
+                "has lowercase": has_lowercase(password),
+                "has symbol": has_symbol(password)}
 
-        hints = {   "long enough": "Your password should be atleast 8 characters long!",
-                    "has digits": "Your password should contain atleast one digit!",
-                    "has uppercase": "Your password should contain atleast one uppercase letter",
-                    "has lowercase": "Your password should contain atleast one lowercase letter",
-                    "has symbol": "Your password should contain atleast one symbol from !@#$%^&*"}
+    hints = {   "long enough": "Your password should be atleast 8 characters long!",
+                "has digits": "Your password should contain atleast one digit!",
+                "has uppercase": "Your password should contain atleast one uppercase letter",
+                "has lowercase": "Your password should contain atleast one lowercase letter",
+                "has symbol": "Your password should contain atleast one symbol from !@#$%^&*"}
         
-        # Takes the results and sums them to and integer for determining the score.
-        score = sum(results.values())
+    # Takes the results and sums them to and integer for determining the score.
+    score = sum(results.values())
 
-        match score:
-            case 0:
-                print("Your password can't be accepted!")
-            case 1:
-                print("Your password is very weak!")
-            case 2:
-                print("Your password is weak!")
-            case 3:
-                print("Your password is ok")
-            case 4:
-                print("Your password is strong!")
-            case 5:
-                print("Your password is very strong!")
+    match score:
+        case 0:
+            print("Your password can't be accepted!")
+        case 1:
+            print("Your password is very weak!")
+        case 2:
+            print("Your password is weak!")
+        case 3:
+            print("Your password is ok")
+        case 4:
+            print("Your password is strong!")
+        case 5:
+            print("Your password is very strong!")
 
-        # Loop for giving the correct hints when some checks return false.
-        for (key, result) in results.items():
-            if result == False:
-                print(hints[key])
+    # Loop for giving the correct hints when some checks return false.
+    for (key, result) in results.items():
+        if not result:
+            print(hints[key])
 
 # Loop the program until user quits, allowing for multiple password checks.
 while True:
@@ -114,7 +103,7 @@ while True:
     if pswd == "q":
         break
 
-    main(pswd)
+    check_password(pswd)
 
 
     
